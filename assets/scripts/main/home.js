9a677839d58f5168ff3af256c454a0138a732694
lidasong <1584809032@qq.com>
2015-02-04 17:17:20 +0800

    js

--- assets/scripts/main/home.js
+++ assets/scripts/main/home.js
@@ -1,18 +1,17 @@
 require([
     'jquery',
-    'dewplayer',
     'bootstrap_dropdown'
 ], function($) {
     'use strict';
 
-    var $dewPlayer = $('#dewplayer'),
-    	$btnPrev = $('.prev'),
-    	$btnNext = $('.next');
-    $btnPrev.on('click',function(){
-    	$dewPlayer.dewprev();
+    var dewPlayer = $('#dewplayer'),
+        $btnPrev = $('.prev'),
+        $btnNext = $('.next');
+    $btnPrev.on('click', function() {
+        console.log(dewPlayer);
+        dewPlayer.dewprev();
     });
-    $btnNext.on('click',function(){
-    	$dewPlayer.dewnext();
+    $btnNext.on('click', function() {
+        dewPlayer.dewnext();
     });
-    console.dir($dewPlayer);
 });
