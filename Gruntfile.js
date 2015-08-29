573ae750b1918f7eb32f25f26a1c7f39c6caaa5d
lidasong <lidasong2014@163.com>
2015-08-29 13:12:05 +0800

    requirejs文件打包压缩

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,15 +8,15 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
 
         /* jsdoc生成js文档 */
-        jsdoc:{
-        	dist:{
-        		src:[ 'assets/scripts/test.js'],
-        		options:{
-        			destination:'docs'
-        		}
-        	}
+        jsdoc: {
+            dist: {
+                src: ['assets/scripts/test.js'],
+                options: {
+                    destination: 'docs'
+                }
+            }
         },
-       
+
         /* JS代码检查 */
         jshint: {
             all: {
@@ -79,17 +79,38 @@ module.exports = function(grunt) {
                 files: [ASSETS_DIR + 'less/lib/bootstrap/**/*.less'],
                 tasks: ['less:bootstrap']
             }
+        },
+        /*
+         * require配置详解
+         * {String name} 合并主文件
+         * {String out} 生成合并文件目录
+         * {String mainConfigFile} requirejs的配置项
+         */
+        requirejs: {
+            compile: {
+                options: {
+                    baseUrl: 'assets/scripts',
+                    mainConfigFile: 'config/config.js',
+                    name: 'main/home',
+                    out: 'assets/scripts/build/build.min.js',
+                    done: function(done, output) {
+                        console.log(output);
+                    }
+                }
+            }
         }
     });
-	grunt.loadNpmTasks('grunt-jsdoc');
+    grunt.loadNpmTasks('grunt-jsdoc');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-csslint');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
     grunt.registerTask('default', ['less', 'watch']);
     grunt.registerTask('js-hint', 'JS代码质量检查', ['jshint']);
     grunt.registerTask('css-lint', 'CSS代码质量检查', ['csslint']);
     grunt.registerTask('minify-css', 'CSS代码压缩', ['cssmin']);
-    grunt.registerTask('doc','jsdoc生成js文档',['jsdoc']);
+    grunt.registerTask('doc', 'jsdoc生成js文档', ['jsdoc']);
+    grunt.registerTask('require-test', 'requirejs合并测试', ['requirejs']);
 };
