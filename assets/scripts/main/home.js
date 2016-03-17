e65df5d1dd773e90dc64062a65b12b26fdcb5dd3
lidasong <lidasong2014@163.com>
2016-03-17 16:02:48 +0800

    修改个人信息

--- assets/scripts/main/home.js
+++ assets/scripts/main/home.js
@@ -23,7 +23,7 @@ require([
         //头像
         console.log('%c','margin-left:100px;padding:80px;border-radius:50%;background:#333 url(http://itec-lds.github.io/musics/assets/images/mugong.jpg) no-repeat center;line-height:200px;');
         //求工作
-        console.log('%c求前端工作——求收留,有小伙伴看到求介绍','font-size:24px;color:#f75a53;text-shadow:0px 0px 0px #fff,1px 1px 0px #b3b3b3;');
+        console.log('%c网易前端工程师一枚；有好去处求收留','font-size:24px;color:#f75a53;text-shadow:0px 0px 0px #fff,1px 1px 0px #b3b3b3;');
     }
      stampInfo();
 });
